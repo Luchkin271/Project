9224a72198c7cfcf9a2b3e8ec39a9e79f34d2545
D. Resnyanskiy <resnyanskiy05@gmail.com>
2024-05-22 00:09:34 +0500

    Deleted 'ggs' folder

--- ggs/js/main.js
+++
@@ -1,9 +0,0 @@
-const objectName = document.querySelector('.content__header_name');
-const objectType = document.querySelector('.content__header_type');
-
-const contentLoad = (name,  type) => {
-    objectName.innerHTML = name;
-    objectType.innerHTML = type;
-}
-
-document.addEventListener('DOMContentLoaded', contentLoad('Имя объекта', 'Тип объекта'))
