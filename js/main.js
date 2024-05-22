584ecb9e1b02282e715969184db706639abb2e5c
Grigoriy76 <luchkin.grigoriy.a@gmail.com>
2024-05-22 18:50:39 +0300

    change main js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 const objectName = document.querySelector('.content__header_name');
 const objectType = document.querySelector('.content__header_type');
+const objectImg = document.querySelector('.content__header_img');
+const objectInfo = document.querySelector('.content__main_list');
+const objectNav = document.querySelector('.content__nav_list');
 
-const contentLoad = (name,  type) => {
+const contentLoad = (name,  type, imgPath) => {
     objectName.innerHTML = name;
     objectType.innerHTML = type;
 }
