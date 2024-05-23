14c9af37fdfbe361633b82a1b4c52e2e50cbda3d
D. Resnyanskiy <resnyanskiy05@gmail.com>
2024-05-24 04:52:19 +0500

    Изменение дизайна. Добавление закреплённого сотрудника через JS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const objectName = document.querySelector('.content__header_name');
 const objectType = document.querySelector('.content__header_type');
+const objectPerson = document.querySelector('.content__header_person');
 const objectImg = document.querySelector('.content__header_img');
 const objectInfo = document.querySelector('.content__main_list');
 const objectNav = document.querySelector('.content__nav_list');
 
-const contentLoad = (name,  type, imgPath) => {
+const contentLoad = (name,  type, personName) => {
     objectName.innerHTML = name;
     objectType.innerHTML = type;
+    objectPerson.innerHTML = personName;
 }
 
-document.addEventListener('DOMContentLoaded', contentLoad('Имя объекта', 'Тип объекта'))
+document.addEventListener('DOMContentLoaded', contentLoad(
+    'Имя объекта: FT1-465',
+    'Тип объекта: Станок',
+    'Закреплённый сотрудник: Лучкин Г.А.'))
